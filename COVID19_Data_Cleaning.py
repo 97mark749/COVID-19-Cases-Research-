067e50a29a8599f5f339b0d2781698d027ee8629
97mark749 <markkirby27@gmail.com>
2020-06-10 22:13:45 -0400

    created dataframes from United States and gobal information

--- COVID19_Data_Cleaning.py
+++ COVID19_Data_Cleaning.py
@@ -1,5 +1,12 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
+
+# STP = standard temperature and pressure
+# SLP = Sea Level Pressure
+# rh = relative Humidity
+# prcp = Precipitation
+
+
 
 class country:
     def __init__(self, name, population, total_cases, total_deaths):
@@ -20,21 +27,42 @@ class state:
         self.total_deaths = total_deaths
 
 class county:
-    def __init__(self, name, population, total_cases, case_timeline,total_deaths):
+    def __init__(self, name, state, population, total_cases, case_timeline,total_deaths):
         self.name = name
         self.population = population
+        self.state = state
         self.total_cases = total_cases
         self.case_timeline = case_timeline
         self.total_deaths = total_deaths
 
-def clean_data():
-    temp_data = 
 
+
+def clean_data():
+    # Create dataframe and select United States Only
+    # Drop unneeded Columns
+    us_temp_data = pd.read_csv('training_data_with_weather_info_week_1.csv')
+    us_temp_data.drop(['Id', 'day_from_jan_first', 'min', 'max', 'stp', 'slp', 'dewp', 'ah', 'wdsp', 'fog'], axis=1, inplace = True)   
+    us_Temp = us_temp_data[us_temp_data['Country'] == 'US']
+
+    # us_states_covid19_daily
+    # us_counties
+    
+    
+    # Create Dataframe and drop unneeded columns
+    # Note: Active means infected but still alive
+    global_dly_data = pd.read_csv('covid_19_clean_complete.csv')
+    global_dly_data.drop(['Province/State', 'Lat', 'Long'], axis = 1, inplace = True)
+    print(global_dly_data.head())
+    
+    # Information as of 6/9/2020
+    # Global Totals
+    g_total_data = pd.read_csv('worldometer_data.csv')
+    g_total_data.drop(['NewCases', 'NewDeaths', 'NewRecovered'], axis = 1, inplace = True)
+    print(g_total_data.head())
 
 
 
 
 if __name__ == "__main__":
-    # Example
-    america = country("United States", 1000000, 23000, 200)
-    print(america)
+
+    clean_data()
